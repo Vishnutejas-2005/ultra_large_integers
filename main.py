354e6baf7b58fd1ff9c123cef667d294ab69fd35
Vishnutejas-2005 <144125542+Vishnutejas-2005@users.noreply.github.com>
2024-12-13 19:13:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from count_pairs import count_pairs_file
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
         print(f'Run {sys.argv[0]} with arguments <filename>')
         sys.exit(1)
     # Open the file argv[1]
